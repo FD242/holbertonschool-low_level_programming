b6089cedae31b4167a24cb959656e99f3a3d2900
Your Name <you@example.com>
2025-03-13 14:33:43 +0000

    a

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - sets memory to array
+ * @s: array
+ * @b: value
+ * @n: n
+ * Return: value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
